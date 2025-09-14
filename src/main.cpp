c532547d73b5577d40aa2173d1dc9d01d458f400
ChristiaanHulstein <christiaanhulstein@gmail.com>
2025-09-14 10:13:06 +0200

    met LED's

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <FastLED.h>
 #include "OneButton.h"
 
-#define Button 8      //internal pull-up
+
+#define Button 2      
 #define M_PWM 3
 #define M_IN1 4
 #define M_IN2 5
@@ -11,9 +12,10 @@
 OneButton button(Button,true);
 
 #define MAX_SPEED 200 //max speed of motor
-#define MS_TOPSPEED 3000 //ms till top speed is reached
-#define MS_TOZERO 2000 //ms to go from top speed to zero
-#define MODES [/*normal*/2,5,1,/*fast*/1,3,0,/*slow*/3,7,2] //array with different modes for random spin time in seconds
+#define MS_TOPSPEED 5000 //ms till top speed is reached
+#define MS_TOZERO 10000 //ms to go from top speed to zero
+#define MODES [/*normal*/6,8,1,/*fast*/1,3,0,/*slow*/3,7,2] //array with different modes for random spin time in seconds
+// 0 = min_random , 1 = max_random , 2 = time in stop mode
 
 enum class MotorState { //setting up all possible states execpt error
     M_Wait,
@@ -38,6 +40,7 @@ static LEDState LED_State = LEDState::LEDcomb1;
 
 static int currentSpeed = 0;
 int easingMode = 1; // 0=Linear, 1=Quadratic, 2=Cubic, 3=Exponential
+int selectedMode = 0; //select mode from MODES array
 
 
 unsigned long softstartStartTime = 0;
@@ -48,7 +51,7 @@ unsigned long randomSpinTime = 3000;
 unsigned long stopStartTime = millis();
 unsigned long errorStartTime = 0;
 bool errorActive = false;
-
+boolean lightsOn = true;
 
 boolean pressed = false;
 
@@ -122,18 +125,67 @@ boolean softEndFunction() {
 
 
 void doubleClick(){
-  selectedMode += 1; 
-  Serial.println("doubleClick");
+  easingMode = easingMode + 1; 
+  //Serial.println("doubleClick");
 }
 
 void singleClick(){
   pressed = true;
-  Serial.println("singleClick");
+  //Serial.println("singleClick");
 }
 
 void longClick(){
-  Serial.println("longClick");
+  //Serial.println("longClick");
+  toggleLights(!lightsOn);
+
+
 }
+
+void setPreset(int preset) {
+  Serial.print("{\"ps\":");
+  Serial.print(preset);
+  Serial.println("}");
+}
+
+void setPlaylist(int playlist) {
+  Serial.print("{\"pl\":");
+  Serial.print(playlist);
+  Serial.println("}");
+}
+
+void setBrightness(int brightness) {
+  Serial.print("{\"bri\":");
+  Serial.print(brightness);
+  Serial.println("}");
+}
+void setColor(int r, int g, int b) {
+  Serial.print("{\"seg\":[{\"col\":[[");
+  Serial.print(r);
+  Serial.print(",");
+  Serial.print(g);
+  Serial.print(",");
+  Serial.print(b);
+  Serial.println("]]}]}");
+}
+
+void setEffect(int fx, int sx, int ix) {
+  Serial.print("{\"seg\":[{\"fx\":");
+  Serial.print(fx);
+  Serial.print(",\"sx\":");
+  Serial.print(sx);
+  Serial.print(",\"ix\":");
+  Serial.print(ix);
+  Serial.println("}]}");
+}
+
+void toggleLights(boolean toggle) {
+  lightsOn = toggle;
+  Serial.print("{\"on\":");
+  Serial.print(toggle ? "true" : "false");
+  Serial.println("}");
+}
+
+
 
 void setup() {
   pinMode(Button, INPUT_PULLUP);
@@ -146,7 +198,7 @@ void setup() {
   button.attachDoubleClick(doubleClick);
   button.attachClick(singleClick);
   button.attachLongPressStop(longClick);
-  int selectedMode = 0 //select mode from MODES array
+
 
    // Default to 3 seconds
 
@@ -160,62 +212,65 @@ void loop() {
   button.tick();
   switch(M_State) { //setting the motor states
 
-    case MotorState::M_Wait:
+    case MotorState::M_Wait: // wachten tot er op de knop gedrukt wordt
       Serial.println("In wait state");
+      LED_State = LEDState::LEDcomb1;
+
       if(pressed == true){
         M_State = MotorState::M_Starting;
         LED_State = LEDState::LEDcomb1;
-        pressed == false;
+        pressed = false;
       }
       
       break;
 
-    case MotorState::M_Starting:
+    case MotorState::M_Starting: //motor langzaam opstarten om piek te voorkomen
       Serial.println("In starting state");
       if(SoftstartFunction() == true){
         M_State = MotorState::M_Running;
         LED_State = LEDState::LEDcomb2;
         startTimeRunning = millis();
-        randomSpinTime = random(3*SELECTED_MODE +1, 3*SELECTED_MODE +2)*1000; // Random duration between 5 and 15 seconds
+        randomSpinTime = random((3*selectedMode + 0)*1000, (3*selectedMode +1)*1000); // Random duration between 5 and 15 seconds
       }
 
       break;
   
-    case MotorState::M_Running:
+    case MotorState::M_Running: // random tijd at hij max snelheid aanhoud
       Serial.println("In running state");
       if (millis() - startTimeRunning >= randomSpinTime) { //after random seconds switch to next state
         M_State = MotorState::M_SpinFreely;
       }
       break;
 
-    case MotorState::M_SpinFreely:
+    case MotorState::M_SpinFreely:      //Om de motor op een leuke manier af te remmen
       Serial.println("In spin freely state");
       
-      if(softEndFunction){ 
+      if(softEndFunction()){ 
         M_State = MotorState::M_Stop;
         stopStartTime = millis();
       }
       break;
 
-    case MotorState::M_Stop:
+    case MotorState::M_Stop:    //om lEDS te laten blinken
       Serial.println("In stop state");
       digitalWrite(M_IN1, LOW); //stop motor
       digitalWrite(M_IN2, LOW);
       LED_State = LEDState::LEDcomb3;
-      if(millis() - stopStartTime >= 3*SELECTED_MODE +3){ //after x seconds switch to wait state
+      if(millis() - stopStartTime >= (3*selectedMode +2)*1000){ //after x seconds switch to wait state
         M_State = MotorState::M_Wait;
+        pressed = false;
       }
       break;
   
     default:  //Error state
-      Serial.println("In error state");
-      if(errorActive = false){
+      //Serial.println("In error state");
+      if(errorActive == false){
         errorActive = true;
         errorStartTime = millis();
       }
       
       LED_State = LEDState::Error;
-      if(millis() - errorStartTime >= 15000){ //after 15 seconds switch to wait state
+      if(millis() - errorStartTime >= 10000){ //after 15 seconds switch to wait state
         digitalWrite(M_IN1, LOW); //stop motor
         digitalWrite(M_IN2, LOW);
       } else {
@@ -228,19 +283,19 @@ void loop() {
   switch (LED_State)   //setting the LED states
   {
     case LEDState::LEDcomb1:
-      // Your code here
+      setPlaylist(1);
       break;
 
     case LEDState::LEDcomb2:
-      // Your code here
+      setPlaylist(2);
       break;
 
     case LEDState::LEDcomb3:
-      // Your code here
+      setPlaylist(3);
       break;
     
     case LEDState::Error:
-      // Rode snel knipperende lichten
+      setPlaylist(4);
       break;
   }
   
